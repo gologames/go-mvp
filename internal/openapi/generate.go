c126b0fd7b5271f3172314788aef54665c64d2cc
gologames <gologames@bk.ru>
2025-05-30 16:10:33 +0200

    lint: initialized golangci-lint config

--- internal/openapi/generate.go
+++ internal/openapi/generate.go
@@ -1,3 +1,4 @@
+// Contains go:generate directive for generating OpenAPI types from schema.
 package openapi
 
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -generate types,spec,skip-prune -package openapi -o openapi.gen.go ../../api/v1/networkconfig.schemas.yml
--- internal/server/generate.go
+++ internal/server/generate.go
@@ -1,3 +1,4 @@
+// Contains go:generate directive for generating OpenAPI server and specification validator.
 package server
 
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -generate types,spec,chi-server -package server -o server.gen.go -import-mapping "./networkconfig.schemas.yml:github.com/gologames/go-mvp/internal/openapi" ../../api/v1/networkconfig.openapi.yml
