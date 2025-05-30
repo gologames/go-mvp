1c0f3d5af9953119f56fae696d61a0f931cd502d
gologames <gologames@bk.ru>
2025-05-30 12:58:42 +0200

    chore: renamed YAML files to .yml

--- internal/cli/generate.go
+++ internal/cli/generate.go
@@ -1,3 +1,3 @@
 package cli
 
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -generate types,client -package cli -o cli.gen.go -import-mapping "./networkconfig.schemas.yaml:github.com/gologames/go-mvp/internal/openapi" ../../api/v1/networkconfig.openapi.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -generate types,client -package cli -o cli.gen.go -import-mapping "./networkconfig.schemas.yml:github.com/gologames/go-mvp/internal/openapi" ../../api/v1/networkconfig.openapi.yml
