4888b9f1ddcd38511deff6ca02aeb049eb27e287
gologames <gologames@bk.ru>
2025-06-09 14:04:25 +0200

    netconfig: added unit tests for validate IP, interface and config

--- internal/networkconfig/networkconfig_test.go
+++ internal/networkconfig/networkconfig_test.go
@@ -1,9 +1,8 @@
-package networkconfig_test
+package networkconfig
 
 import (
 	"testing"
 
-	"github.com/gologames/go-mvp/internal/networkconfig"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -27,8 +26,7 @@ func TestValidateIdentifierCorrect(t *testing.T) {
 	}
 
 	for _, identifier := range identifiers {
-		err := networkconfig.ValidateIdentifier(identifier)
-		assert.NoError(t, err)
+		assert.NoError(t, ValidateIdentifier(identifier))
 	}
 }
 
@@ -48,7 +46,223 @@ func TestValidateIdentifierIncorrect(t *testing.T) {
 	}
 
 	for _, identifier := range identifiers {
-		err := networkconfig.ValidateIdentifier(identifier)
-		assert.Error(t, err)
+		assert.Error(t, ValidateIdentifier(identifier))
+	}
+}
+
+func TestValidateIPCorrect(t *testing.T) {
+	t.Parallel()
+
+	validIPs := []string{
+		"",
+		"127.0.0.1",
+		"::1",
+		"192.168.1.100",
+		"2001:db8::68",
+		"2001:0db8:0000:0000:0000:0000:0000:0068",
+	}
+
+	for _, ip := range validIPs {
+		assert.NoError(t, ValidateIP(ip))
+	}
+}
+
+func TestValidateIPIncorrect(t *testing.T) {
+	t.Parallel()
+
+	invalidIPs := []string{
+		"not_an_ip",
+		"256.256.256.256",
+		"192.168.1.",
+		"2001:db8:::68",
+		"gggg::1",
+		"1234:5678:90ab:cdef:ghij:klmn:opqr:stuv",
+		"2001:db8::68::1",
+	}
+
+	for _, ip := range invalidIPs {
+		assert.Error(t, ValidateIP(ip))
+	}
+}
+
+func TestValidateInterface(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		iface   Interface
+		wantErr bool
+	}
+
+	tests := []testCase{
+		{
+			iface: Interface{
+				Name:    "eth0",
+				Address: "192.168.1.10",
+				Mask:    "255.255.255.0",
+				Gateway: "192.168.1.1",
+			},
+			wantErr: false,
+		},
+		{
+			iface: Interface{
+				Name:    "123", // invalid name
+				Address: "192.168.1.10",
+				Mask:    "255.255.255.0",
+				Gateway: "192.168.1.1",
+			},
+			wantErr: true,
+		},
+		{
+			iface: Interface{
+				Name:    "eth0",
+				Address: "999.999.999.999", // invalid address
+				Mask:    "255.255.255.0",
+				Gateway: "192.168.1.1",
+			},
+			wantErr: true,
+		},
+		{
+			iface: Interface{
+				Name:    "eth0",
+				Address: "192.168.1.10",
+				Mask:    "not_a_mask", // invalid mask
+				Gateway: "192.168.1.1",
+			},
+			wantErr: true,
+		},
+		{
+			iface: Interface{
+				Name:    "eth0",
+				Address: "192.168.1.10",
+				Mask:    "255.255.255.0",
+				Gateway: "bad_gateway", // invalid gateway
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		err := validateInterface(&tt.iface)
+		if tt.wantErr {
+			assert.Error(t, err)
+		} else {
+			assert.NoError(t, err)
+		}
+	}
+}
+
+func TestValidateConfig(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		cfg     NetworkConfig
+		wantErr bool
+	}
+
+	tests := []testCase{
+		{
+			cfg: NetworkConfig{
+				Hostname:   "host1",
+				Interfaces: nil,
+			},
+			wantErr: false,
+		},
+		{
+			cfg: NetworkConfig{
+				Hostname: "host1",
+				Interfaces: []Interface{
+					{
+						Name:    "eth0",
+						Address: "192.168.1.10",
+						Mask:    "255.255.255.0",
+						Gateway: "192.168.1.1",
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			cfg: NetworkConfig{
+				Hostname: "host1",
+				Interfaces: []Interface{
+					{
+						Name:    "eth0",
+						Address: "192.168.1.10",
+						Mask:    "255.255.255.0",
+						Gateway: "192.168.1.1",
+					},
+					{
+						Name:    "eth1",
+						Address: "192.168.1.11",
+						Mask:    "255.255.255.0",
+						Gateway: "192.168.1.1",
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			cfg: NetworkConfig{
+				Hostname: "bad host", // invalid hostname
+				Interfaces: []Interface{
+					{
+						Name:    "eth0",
+						Address: "192.168.1.10",
+						Mask:    "255.255.255.0",
+						Gateway: "192.168.1.1",
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			cfg: NetworkConfig{
+				Hostname: "host1",
+				Interfaces: []Interface{
+					{
+						Name:    "eth0",
+						Address: "192.168.1.10",
+						Mask:    "255.255.255.0",
+						Gateway: "192.168.1.1",
+					},
+					{
+						Name:    "eth1",
+						Address: "192.168.1.11",
+						Mask:    "255.255.255.0",
+						Gateway: "192.168.1.1",
+					},
+					{
+						Name:    "eth2",
+						Address: "192.168.1.12",
+						Mask:    "255.255.255.0",
+						Gateway: "192.168.1.1",
+					},
+				},
+			},
+			wantErr: true, // more than maxInterfaceCount
+		},
+		{
+			cfg: NetworkConfig{
+				Hostname: "host1",
+				Interfaces: []Interface{
+					{
+						Name:    "123", // invalid interface
+						Address: "192.168.1.10",
+						Mask:    "255.255.255.0",
+						Gateway: "192.168.1.1",
+					},
+				},
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		err := validate(&tt.cfg)
+		if tt.wantErr {
+			assert.Error(t, err)
+		} else {
+			assert.NoError(t, err)
+		}
 	}
 }
